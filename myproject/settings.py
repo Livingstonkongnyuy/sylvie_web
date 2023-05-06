2e1c701f2d28095aea15f83a696ff264de922721
Kongnyuy Livingston <99125408+Livingstonkongnyuy@users.noreply.github.com>
2023-05-06 08:43:40 -0700

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -132,8 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-
 # STATICFILES_DIRS=[BASE_DIR/'static']
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
